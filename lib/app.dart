3ea7538d9b7b609d537f6681bbb5868aa6fbc982
Shahaiar Khaled <mdkhaled199913@gmail.com>
2026-07-08 12:37:44 +0600

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_pay/core/widgets/mobile_pay_app_bar.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Center(child: Text("Mobile Pay"),),)
+      ),
+    );
+
+  }
+}
+
